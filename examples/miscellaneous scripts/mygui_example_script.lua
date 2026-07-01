9573bf33cb0e15b90ff7aacd5d514295e6a36544
Genpretz <156712049+Genpretz@users.noreply.github.com>
2026-07-01 02:10:39 -0700

    fix: update script to use correct global for MyGUI related methods

--- examples/miscellaneous scripts/mygui_example_script.lua
+++ examples/miscellaneous scripts/mygui_example_script.lua
@@ -2,16 +2,16 @@
 -- Save this file as a lua script to be loaded by the mod loader
 
 -- 1. Create a Main Window
--- KenshiLua.createWindow(skin, x, y, width, height, name, parentOrLayer)
+-- MyGUI.createWindow(skin, x, y, width, height, name, parentOrLayer)
 -- We use layer "Window" if there's no parent.
 -- Kenshi skins usually prefix with "Kenshi_"
-local mainWindow = KenshiLua.createWindow("Kenshi_WindowCX", 100, 100, 400, 400, "ExampleWindow", "Window")
+local mainWindow = MyGUI.createWindow("Kenshi_WindowCX", 100, 100, 400, 400, "ExampleWindow", "Window")
 mainWindow:setCaption("MyGUI Example Window")
 mainWindow:setMovable(true)
 
 -- 2. Create a Button inside the Window
 -- We pass `mainWindow` as the 7th argument (parent)
-local myButton = KenshiLua.createButton("Kenshi_Button1", 20, 40, 150, 30, "ExampleButton", mainWindow)
+local myButton = MyGUI.createButton("Kenshi_Button1", 20, 40, 150, 30, "ExampleButton", mainWindow)
 myButton:setCaption("Click Me!")
 
 -- Register a click callback
@@ -23,7 +23,7 @@ myButton:registerCallback("click", function(widget)
 end)
 
 -- 3. Create an EditBox (Text Input)
-local editBox = KenshiLua.createEditBox("Kenshi_EditBox", 20, 90, 200, 30, "ExampleEditBox", mainWindow)
+local editBox = MyGUI.createEditBox("Kenshi_EditBox", 20, 90, 200, 30, "ExampleEditBox", mainWindow)
 editBox:setCaption("Type something here...")
 editBox:setEditMultiLine(false)
 
@@ -35,7 +35,7 @@ editBox:registerCallback("change", function(widget)
 end)
 
 -- 4. Create a ListBox
-local listBox = KenshiLua.createListBox("Kenshi_ListBox", 20, 140, 200, 150, "ExampleListBox", mainWindow)
+local listBox = MyGUI.createListBox("Kenshi_ListBox", 20, 140, 200, 150, "ExampleListBox", mainWindow)
 listBox:addItem("First Item")
 listBox:addItem("Second Item")
 listBox:addItem("Third Item")
@@ -52,7 +52,7 @@ listBox:registerCallback("change", function(widget)
 end)
 
 -- 5. Creating a generic TextBox (Label)
-local label = KenshiLua.createTextBox("Kenshi_TextboxPaintedText", 20, 310, 300, 20, "ExampleLabel", mainWindow)
+local label = MyGUI.createTextBox("Kenshi_TextboxPaintedText", 20, 310, 300, 20, "ExampleLabel", mainWindow)
 label:setCaption("This is a simple text label.")
 -- We can set its text alignment or color using properties
 label:setProperty("TextAlign", "Left VCenter")
@@ -62,7 +62,7 @@ label:setProperty("TextColour", "1 0 0") -- Red text
 mainWindow:setVisibleSmooth(true)
 
 -- To clean up later or close the window, you could bind a close button:
-local closeBtn = KenshiLua.createButton("Kenshi_Button1", 250, 310, 100, 30, "CloseButton", mainWindow)
+local closeBtn = MyGUI.createButton("Kenshi_Button1", 250, 310, 100, 30, "CloseButton", mainWindow)
 closeBtn:setCaption("Close")
 closeBtn:registerCallback("click", function(widget)
     mainWindow:destroySmooth()
