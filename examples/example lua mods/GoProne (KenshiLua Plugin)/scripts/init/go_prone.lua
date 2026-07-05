b7f4645f5b56eb5094d66b719ffd65ad4f0392d8
Genpretz <156712049+Genpretz@users.noreply.github.com>
2026-07-05 16:44:24 -0700

    feat: multiple characters can be selected at the same time when entering the ProneState; Calls to log changed to logDebug;

--- examples/example lua mods/GoProne (KenshiLua Plugin)/scripts/init/go_prone.lua
+++ examples/example lua mods/GoProne (KenshiLua Plugin)/scripts/init/go_prone.lua
@@ -1,44 +1,55 @@
 -- go_prone.lua
-local log = KenshiLua.log
+local logDebug = KenshiLua.logDebug
 local KC_V = 47
 
 local function toggle_prone()
-    log("[Go Prone] toggle_prone called")
+    logDebug("[Go Prone] toggle_prone called")
 
     local world = getGameWorld()
     if not world then
-        log("[Go Prone] No world")
+        logDebug("[Go Prone] No world")
         return
     end
 
-    local c = getPlayerInterface().selectedCharacter:getCharacter()
-    if not c then
-        log("[Go Prone] No selected character")
+    local selected = getPlayerInterface().selectedCharacters
+    if not selected then
+        logDebug("[Go Prone] No selected characters set found")
         return
     end
 
-    local prone_state = c:getProneState()
-    log("[Go Prone] Current prone state: " .. tostring(prone_state))
+    local count = 0
+    for h, _ in pairs(selected:toTable()) do
+        local c = h:getCharacter()
+        if c then
+            count = count + 1
+            local prone_state = c:getProneState()
+            logDebug("[Go Prone] Character " .. tostring(count) .. " current prone state: " .. tostring(prone_state))
 
-    local prone_state = c:getProneState()
-
-    if prone_state == ProneState.PS_NORMAL then
-        log("[Go Prone] Going prone")
-        c:setProneState(ProneState.PS_STAYING_LOW)
+            if prone_state == ProneState.PS_NORMAL then
+                logDebug("[Go Prone] Going prone")
+                c:setProneState(ProneState.PS_STAYING_LOW)
+            else
+                logDebug("[Go Prone] Leaving prone")
+                c:setProneState(ProneState.PS_NORMAL)
+            end
+        end
+    end
+    
+    if count == 0 then
+        logDebug("[Go Prone] No valid selected characters")
     else
-        log("[Go Prone] Leaving prone")
-        c:setProneState(ProneState.PS_NORMAL)
+        logDebug("[Go Prone] Toggled prone state for " .. tostring(count) .. " characters")
     end
 end
 
 local function on_key_down(key_code)
-    log("[Go Prone] Key pressed: " .. tostring(key_code))
+    logDebug("[Go Prone] Key pressed: " .. tostring(key_code))
     if key_code == KC_V then
-        log("[Go Prone] V key detected, toggling prone")
+        logDebug("[Go Prone] V key detected, toggling prone")
         toggle_prone()
     end
 end
 
-log("[Go Prone] Registering key callback")
+logDebug("[Go Prone] Registering key callback")
 registerHandler("onKeyDown", on_key_down)
-log("[Go Prone] Callback registered")
+logDebug("[Go Prone] Callback registered")
